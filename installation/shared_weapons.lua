2b2ab42abe4acf924cedc1516864ba66bcd1cf49
Andyauk <47583709+Andyauk@users.noreply.github.com>
2023-12-24 02:24:19 +0000

    Update shared_weapons.lua

--- installation/shared_weapons.lua
+++ installation/shared_weapons.lua
@@ -1,5 +1,5 @@
 ---------------
-Insert below into qr-core/shared/weapons.lua
+Insert below into rsg-core/shared/weapons.lua
 ----------------
 
 	[`weapon_fishingrod`]	= {['name'] = 'weapon_fishingrod',	['attachPoint'] = 13,	['label'] = 'Fishing Rod',	['weight'] = 500,	['type'] = 'weapon',	['ammotype'] = nil,	['image'] = 'weapon_fishingrod.png',	['unique'] = true,	['useable'] = true,	['level'] = 0,	['description'] = 'Placeholder'},
